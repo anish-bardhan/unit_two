30a8a8373be5e2dac12c8455f4200fe6c5b6bc7f
Anish Bardhan <2020abardhan@dwight.edu>
2018-10-15 13:12:58 -0400

    init

---
+++ year_one_unit_2/projects/my_project.py
@@ -0,0 +1,24 @@
+print("styles: astro, evolve, spongebob, littlered, or face")
+print("sizes: extra small, small, medium, large, or extra large")
+print("colors: black, white, green, red, yellow, blue, orange, or purple")
+print("order days: one, two, three, four, or five")
+
+def order(int):
+    style = input("what is the custom style?")
+    size = input("what is the custom size?")
+    color = input("what is the custom color?")
+    days = input("how much time do you have to make the order?")
+    order = (style, size, color, days)
+    print(order)
+
+order(7)
+
+def cost(arr):
+
+
+def price(num):
+    price = 0
+    price += num
+    price += 5
+    print(price)
+price(5)
